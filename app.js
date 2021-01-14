3920d0b01bf680132bcae123a8d13b35cff860e3
Khuashal <bharathabk84@gmail.com>
2021-01-15 03:44:15 +0530

    vue events

--- app.js
+++ app.js
@@ -1,34 +1,9 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Finsh the course and learn Vue!',
-      courseGoalA: 'Finsh the course and learn Vue!',
-      courseGoalB: 'Master Vue and build amazing apps!',
-      courseGoalC: '<h2>Master Vue and build amazing apps!</h2>',
-      vueLink: 'https://www.google.com/',
+      counter: 0,
     };
-  },
-  methods: {
-    outputGoal() {
-      const randomNumber = Math.random();
-      if (randomNumber < 0.5) {
-        return 'Learn Vue!';
-      } else {
-        return 'Master Vue!';
-      }
-    },
-    newOutputGoal() {
-      const randomNumber = Math.random();
-      if (randomNumber < 0.5) {
-        return this.courseGoalA;
-      } else {
-        return this.courseGoalB;
-      }
-    },
-    OutputGoalC() {
-      return this.courseGoalC;
-    },
   },
 });
 
-app.mount('#user-goal');
+app.mount('#events');
