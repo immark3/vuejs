c2d8d902b1bd5b816849b249c625bcfad3f08058
Khuashal <bharathabk84@gmail.com>
2021-01-14 23:39:25 +0530

    newapp js updated

---
+++ newapp.js
@@ -0,0 +1,33 @@
+// console.log("WWWWW")
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal()
+// {
+    
+//     const enteredValue=inputEl.value;
+//     const listItemEl=document.createElement('li');
+//     listItemEl.textContent= enteredValue;
+//     console.log("enteredValue = ",enteredValue.length)
+//     if(enteredValue.length>0)
+//     {
+//         listEl.appendChild(listItemEl)
+//         inputEl.value="";
+//     }
+    
+    
+// }
+
+// buttonEl.addEventListener('click',addGoal)
+
+
+Vue.createApp({
+    data(){
+        return{
+            goals:[],
+            enteredValue:''
+        };
+    }
+
+});
