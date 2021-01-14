60222878f9f75d01a49f670f3db0390425603af1
Khuashal <bharathabk84@gmail.com>
2021-01-15 00:22:18 +0530

    vue js updated

--- newapp.js
+++ newapp.js
@@ -1,23 +1,3 @@
-// console.log("WWWWW")
-// const buttonEl = document.querySelector('button');
-// const inputEl = document.querySelector('input');
-// const listEl = document.querySelector('ul');
-
-// function addGoal()
-// {
-    
-//     const enteredValue=inputEl.value;
-//     const listItemEl=document.createElement('li');
-//     listItemEl.textContent= enteredValue;
-//     console.log("enteredValue = ",enteredValue.length)
-//     if(enteredValue.length>0)
-//     {
-//         listEl.appendChild(listItemEl)
-//         inputEl.value="";
-//     }
-    
-    
-// }
 
 // buttonEl.addEventListener('click',addGoal)
 
@@ -28,6 +8,13 @@ Vue.createApp({
             goals:[],
             enteredValue:''
         };
+    },
+    methods: {
+        addGoal()
+        {
+            this.goals.push(this.enteredValue);
+            this.enteredValue='';
+        }
     }
 
-});
+}).mount('#app');
