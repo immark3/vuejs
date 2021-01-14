f92fe5a5563bc4dff07b88ed18eb2cd274e38216
Khuashal <bharathabk84@gmail.com>
2021-01-15 03:58:19 +0530

    vue events with methods

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    add() {
+      this.counter = this.counter + 1;
+    },
+    remove() {
+      this.counter = this.counter - 1;
+      if (this.counter < 0) {
+        this.counter = 0;
+      }
+    },
+  },
 });
 
 app.mount('#events');
