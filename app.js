3df87f5bc0a3700bb146913adc7defc4720d2fe4
Khuashal <bharathabk84@gmail.com>
2021-01-14 22:49:56 +0530

    app js updated

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+console.log("WWWWW")
 const buttonEl = document.querySelector('button');
 const inputEl = document.querySelector('input');
 const listEl = document.querySelector('ul');
 
 function addGoal()
 {
-
+    
+    const enteredValue=inputEl.value;
+    const listItemEl=document.createElement('li');
+    listItemEl.textContent= enteredValue;
+    console.log("enteredValue = ",enteredValue.length)
+    if(enteredValue.length>0)
+    {
+        listEl.appendChild(listItemEl)
+        inputEl.value="";
+    }
+    
+    
 }
 
 buttonEl.addEventListener('click',addGoal)
